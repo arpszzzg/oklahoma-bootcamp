461468abac14c9d714491e8bdc3f890e0ed54e64
Greg Hawaii <aaaa@gmail.com>
2013-07-01 14:29:57 -0500

    added module with functions

--- add_numb.py
+++ add_numb.py
@@ -1,10 +1,10 @@
 
 #test by balabala
+#from adding import add_two, add_three
 
-def add_two(a,b):
-	return a+b
 
 def main():
+	print ("2 +3 +4 =", add_three(2,3,4) ) 
 	print ("2+3 = ", add_two(2,3)  )
 	print ("2+4 = ", add_two(2,4)  )
 
